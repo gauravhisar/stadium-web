d8af47cbf9d8798266debbf291dec3e39a207037
Sulbha Bansal <sulbha15@gmail.com>
2020-11-12 17:11:46 +0530

    Some files have been merged. Records page has been created everytime a user login or signup.

---
+++ stadium/login/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class event:
+    ev_id=None
+    ev_name=None
+    date=None
+    time=None
+    seats_req=None
+    org_by=None
+    price=None
