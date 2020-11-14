86987ba182e0cf738d1d3ecb9bc789078d9170fb
Sulbha Bansal <sulbha15@gmail.com>
2020-11-15 01:42:56 +0530

    Seat Booking Feature is added Contact page has been made

--- stadium/login/urls.py
+++ stadium/login/urls.py
@@ -7,6 +7,12 @@ urlpatterns =[
     path('verify_user', views.verify_user, name='verify_user'),
     path('records',views.records,name='records'),
     path('signup', views.signup,  name='signup'),
-    path('store_user', views.store_user, name='store_user')
+    path('store_user', views.store_user, name='store_user'),
+    path('events',views.events, name='events'),
+    path('contact',views.contact, name='contact'),
+    path('select_event', views.select_event, name='select_event'),
+    path('login_book',views.login_book, name='login_book'),
+    path('booked',views.booked, name='booked')
+
 
 ]
